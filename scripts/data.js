7952035c04661bc41a23b999c118e357c265f9e2
Jamil Ben Selma <jben@azur.local>
2016-12-20 09:38:56 +0100

    first commit

---
+++ scripts/data.js
@@ -0,0 +1,144 @@
+var cars = [
+  {
+    "index": 0,
+    "Constructeur": "BMW",
+    "Modele": "Modele Mini Citadine",
+    "type": 
+      {
+        "id": 4, "typeName": "Berline"
+      },
+    "option": [
+      "deserunt",
+      "elit",
+      "dolor",
+      "tempor",
+      "deserunt",
+      "est",
+      "reprehenderit"
+    ],
+    "carburant": "Hybrid",
+    "date": "17-09-2015",
+    "prix": "14524 €",
+    "picture": "http://placehold.it/32x32",
+    "description": "Ut incididunt dolor exercitation eiusmod est sit cupidatat incididunt pariatur aute ea proident nostrud sint. Laboris nulla nostrud aliquip elit officia aliquip quis exercitation elit non labore. Sit aliquip consectetur deserunt in cupidatat nostrud eiusmod sit magna et tempor dolore labore Lorem.\r\n"
+  },
+  {
+    "index": 1,
+    "Constructeur": "Renault",
+    "Modele": "Modele Citadine",
+    "type": 
+      {
+        "id": 3, "typeName": "Citadine"
+      },
+    "option": [
+      "ut",
+      "eiusmod",
+      "veniam",
+      "proident",
+      "sunt",
+      "amet",
+      "culpa"
+    ],
+    "carburant": "Essence",
+    "date": "04-06-2016",
+    "prix": "14080 €",
+    "picture": "http://placehold.it/32x32",
+    "description": "Voluptate esse sunt ut magna. Culpa qui quis sint ut exercitation. Cupidatat excepteur Lorem esse magna. Dolor eiusmod ea exercitation irure.\r\n"
+  },
+  {
+    "index": 2,
+    "Constructeur": "Mercedes",
+    "Modele": "Modele 4x4",
+    "type": 
+      {
+        "id":8, "typeName": "4x4"
+      },
+    "option": [
+      "sit",
+      "sunt",
+      "qui",
+      "aliqua",
+      "culpa",
+      "quis",
+      "sit"
+    ],
+    "carburant": "Hybrid",
+    "date": "02-04-2015",
+    "prix": "14821 €",
+    "picture": "http://placehold.it/32x32",
+    "description": "Fugiat ullamco laboris exercitation nulla aliquip minim sint. Exercitation ipsum irure dolore eiusmod Lorem mollit et do eu excepteur anim aliquip consequat. Magna occaecat pariatur aliquip dolore ipsum reprehenderit nulla consectetur qui esse. Reprehenderit officia aute ut ut esse.\r\n"
+  },
+  {
+    "index": 3,
+    "Constructeur": "Citroen",
+    "Modele": "Modele SUV",
+    "type":
+      {
+        "id": 7, "typeName": "SUV"
+      },
+    "option": [
+      "ullamco",
+      "laboris",
+      "amet",
+      "culpa",
+      "Lorem",
+      "do",
+      "eiusmod"
+    ],
+    "carburant": "Diesel",
+    "date": "08-12-2015",
+    "prix": "13647 €",
+    "picture": "http://placehold.it/32x32",
+    "description": "Officia deserunt et eu eu. Deserunt qui eu officia consectetur eiusmod proident commodo non labore dolore et laborum ad laboris. Proident cupidatat ut ut fugiat non duis incididunt magna incididunt quis. Esse ullamco cupidatat sint consectetur consectetur cupidatat proident id adipisicing consequat. Deserunt ea proident pariatur magna culpa officia anim nulla. Dolor et cillum cillum velit cillum ut labore consequat laboris irure ad nisi est.\r\n"
+  },
+  {
+    "index": 4,
+    "Constructeur": "Peugeot",
+    "Modele": "Modele 4x4",
+    "type": 
+      {
+        "id": 8, "typeName": "4x4"
+      },
+    "option": [
+      "aute",
+      "Lorem",
+      "nulla",
+      "consectetur",
+      "pariatur",
+      "cupidatat",
+      "do"
+    ],
+    "carburant": "Hybrid",
+    "date": "22-09-2015",
+    "prix": "13977 €",
+    "picture": "http://placehold.it/32x32",
+    "description": "Sunt ut deserunt amet ex ad ipsum minim amet mollit voluptate dolore magna nulla. Velit eiusmod minim esse pariatur esse id ut dolor ullamco dolore. Non pariatur elit veniam excepteur et excepteur dolore. Commodo eiusmod consectetur est officia nulla culpa non.\r\n"
+  }
+]
+
+var types = [
+    {
+        "id": 1 , "typeName": "Compact"
+    },
+    {
+        "id": 2, "typeName": "Mini Citadine"
+    },
+    {
+        "id": 3, "typeName": "Citadine"
+    },
+    {
+        "id": 4, "typeName": "Berline"
+    },
+    {
+        "id": 5, "typeName": "Sport"
+    },
+    {
+        "id": 6, "typeName": "Roadster"
+    },
+    {
+        "id": 7, "typeName": "SUV"
+    },
+    {
+        "id": 8, "typeName": "4x4"
+    }
+]
---
+++ scripts/main.js
@@ -0,0 +1,241 @@
+﻿$(document).ready( function() {
+    /**
+     * DataSource
+     */
+
+    // $.getJSON('../data.json').success(function(data) {
+    //                 var json = JSON.parse(data)
+    //                 console.log(json);
+    //             });
+
+    //     var jsonData = new kendo.data.DataSource({
+    //         transport: {
+    //             read: {
+    //                 url: "http://localhot:58884/data.json",
+    //                 type: "get",
+    //                 dataType: "json"
+    //             }
+    //         },
+    //         schema: {
+    //                 data: "data"
+    //         }
+    //     });
+
+
+    /**
+     * Splitter : Header
+     */
+    $('#header').kendoSplitter({
+        orientation: "vertical",
+        panes: [ 
+            { size: "90%", resizable: false },
+            { size: "10%",}
+        ]
+    });
+
+
+    /**
+     * Menu
+     */
+    $("#menu").kendoMenu({
+        dataSource:[
+            {
+                text: 'Menu 1',
+                cssClass: "myClass",                         // Add custom CSS class to the item, optional, added 2012 Q3 SP1.
+                url: "http://www.kendoui.com",               // Link URL if navigation is needed, optional.
+                attr: {
+                    custom: 'value',                         // Add attributes with specified values
+                    other: 'value'
+                },
+                items: [{text: "Sub Item 1"},{text: "Sub Item 2"},{text: "Sub Item 2"},{text: "Sub Item 2"}]
+            },
+            {
+                text: 'menu 2',
+                cssClass: "myClass",                         // Add custom CSS class to the item, optional, added 2012 Q3 SP1.
+                url: "http://www.kendoui.com",               // Link URL if navigation is needed, optional.
+                attr: {
+                    custom: 'value',                         // Add attributes with specified values
+                    other: 'value'
+                },
+                items: [{text: "Sub Item 1"},{text: "Sub Item 2"},{text: "Sub Item 2"},{text: "Sub Item 2"}]
+            },
+            {
+                text: 'menu 3',
+                cssClass: "myClass",                         // Add custom CSS class to the item, optional, added 2012 Q3 SP1.
+                url: "http://www.kendoui.com",               // Link URL if navigation is needed, optional.
+                attr: {
+                    custom: 'value',                         // Add attributes with specified values
+                    other: 'value'
+                },
+                items: [{text: "Sub Item 1"},{text: "Sub Item 2"},{text: "Sub Item 2"},{text: "Sub Item 2"}]
+            },
+        ]
+    });
+
+
+    /**
+     * Splitter : Main
+     */
+    $('#main').kendoSplitter({
+        panes: [ { size: "10%", resizable: true },
+                 { size: "90%", resizable: true } 
+        ]
+    });
+
+
+    /**
+     * kendoGrid : Table
+     */
+    var dataCars = new kendo.data.DataSource({
+        pageSize: 20,
+        data: cars,
+        autoSync: true,
+        schema: {
+            model: {
+                id: "index",
+                fields: {
+                    index: { editable: false, nullable: true },
+                    Constructeur: { type: "string", editable: true, nullable: true },
+                    Modele: { type:"string", editable: true, nullable: true },
+                    type: { type:"string", defaultValue: { "id": 1, typeName: "Berline"} },
+                    carburant: { type:"string", editable: true, nullable: true },
+                    date: { type:"string", editable: true, nullable: true },
+                    prix: { type: "number", validation: { required: true, min: 0 } },
+                    picture: { type: "string", validation: { required: true} },
+                }
+            }
+        }
+    });
+
+    $("#grid").kendoGrid({
+            dataSource: dataCars,
+            filterable: true,
+            sortable: true,
+            columns: [
+                { field: "index" },
+                { field: "Constructeur" },
+                { field: "Modele" },
+                { field: "type", editor: categoryDropDownEditor, template: "#=type.typeName#" },
+                { field: "carburant" },
+                { field: "date" },
+                { field: "prix", format: "{0:c}" },
+                { field: "picture" }
+            ],
+            editable: true,
+            dataSource: cars
+        });
+
+    function categoryDropDownEditor(container, options) {
+        $('<input required name="' + options.field + '"/>').appendTo(container).kendoDropDownList({
+                autoBind: false,
+                dataTextField: "typeName",
+                dataValueField: "id",
+                dataSource: { 
+                    data: types
+                }
+        });
+    }
+
+    dataTypes = new kendo.data.HierarchicalDataSource({
+        data: types
+        ,
+        schema: {
+            model: {
+                id: "id",
+                typeName: "typeName"
+            }
+        }
+    });
+
+    $("#treeview").kendoTreeView({
+        dataSource: dataTypes,
+        checkboxes: true,
+        select: onSelect,
+        check: onCheck,
+        change: onChange,
+        collapse: onCollapse,
+        dataValueField: "id",
+        dataTextField: "typeName"
+    });
+
+    function onSelect(e) { 
+        console.log("Selecting: " + this.text(e.node));
+    }
+
+    function onCheck(e) {
+        console.log("Checkbox changed :: " + this.text(e.node));
+    }
+
+    function onChange(e) {
+        console.log("Selection changed");
+    }
+
+    function onCollapse(e) {
+        console.log("Collapsing " + this.text(e.node));
+    }
+
+    // grid.dataSource.originalFilter = grid.dataSource.sort;
+    // console.log(grid.dataSource.originalFilter);
+
+    var grid = $("#grid").data("kendoGrid");
+    console.log(grid);
+    grid.sort('DSC');
+
+
+    /**
+     * Data Form
+     */
+    var dataForm = kendo.observable({
+        culture: "fr-FR",
+        dataGrid: [],
+        first: null,
+        last: null,
+        sexe: null,
+        birth: null,//kendo.toString(new Date(),"d"),
+        displayBirth: function() {
+            var birth = this.get("birth");
+            return kendo.toString(birth,"d");
+        },
+        countries: [
+            { name: "France", value: "france" },
+            { name: "Portugal", value: "portugal" },
+            { name: "Italie", value: "italie" }
+        ],
+        selectedCountry: "France",
+        displayCountry: function() {
+            var country = this.get("selectedCountry");
+            return kendo.stringify(country);
+        },
+        pushDataToGrid: function() {
+                            this.get("dataGrid").push({
+                                                    Prénom: this.get("first"),
+                                                    Nom: this.get("last"),
+                                                    Sexe: this.get("sexe"),
+                                                    Date: this.get("birth"),
+                                                    Pays: this.get("selectedCountry")
+                                                });
+
+                            // reset form fields 
+
+                            this.set("first","");
+                            this.set("last","");
+                            this.set("sexe","");
+                            this.set("birth","");
+                            this.set("selectedCountry","France");
+                        },
+    });
+
+    dataForm.bind("change", function (e) {
+
+        if (e.field == "birth") {
+            dataForm.set("birth", kendo.toString(e.sender[e.field], "d"));
+        }
+
+        console.log(e.field); // will output the field name when the event is raised
+    });
+
+    var json = dataForm.toJSON();
+
+    kendo.bind(document.body.children, dataForm);
+
+});
